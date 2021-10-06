0799f24cf9683d583107262986e83f7457d7bed0
Maria <msarmi84@hotmail.com>
2021-10-06 16:46:22 +0200

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,8 +11,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  fullName = new FormControl('', [Validators.required, Validators.required]);
+  phone = new FormControl('', [Validators.required, Validators.required]);
+
+  constructor(public dialog: MatDialog) {}
+
+
   showHide() {
 
   }
   isShowHideFlag = "over"
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    else if (this.fullName.hasError('required')){
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  openDialog() {
+    this.dialog.open(DialogElementsExampleDialog);
+  }
 }
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  templateUrl: 'dialog-elements-example-dialog.html',
+})
+export class DialogElementsExampleDialog {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, DialogElementsExampleDialog } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatDialogModule, MatFormFieldModule} from '@angular/material';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule} from '@angular/forms';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
-  ],
+    DialogElementsExampleDialog
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,8 +32,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSidenavModule
+    MatSidenavModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatDialogModule
   ],
+  entryComponents: [DialogElementsExampleDialog],
+
   providers: [],
   bootstrap: [AppComponent]
 })
